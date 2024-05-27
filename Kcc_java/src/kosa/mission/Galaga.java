144d702348934deff5ffaa5f67c64339b04a0fc7
Cheol-Cheol <dream174822@gmail.com>
2024-05-27 17:11:48 +0900

    240527 JAVA

--- Kcc_java/src/kosa/mission/Galaga.java
+++ Kcc_java/src/kosa/mission/Galaga.java
@@ -7,7 +7,6 @@ public class Galaga {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		int T = Integer.parseInt(sc.nextLine());
-		int num = 0;
 
 		for (int i = 1; i <= T; i++) {
 			int A = Integer.parseInt(sc.next());
