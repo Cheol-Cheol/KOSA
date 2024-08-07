441ec3bc627c89c2ef42aba4a9c51b3d284423a9
Cheol-Cheol <dream174822@gmail.com>
2024-08-07 17:12:53 +0900

    240807 SPRING LEGACY

---
+++ Kcc_springmvc/ex00/src/main/java/org/zerock/domain/SampleDTO.java
@@ -0,0 +1,17 @@
+package org.zerock.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+// VO 객체 vs DTO 객체
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class SampleDTO {
+
+	private String name;
+
+	private int age;
+
+}
